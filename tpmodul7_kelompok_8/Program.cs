1076b20cb11f4b1d9d968960e5655676abe7b299
rafliad <radenistri@gmail.com>
2024-03-31 21:44:15 +0700

    menambahkan class KuliahMahasiswa1302223153

--- tpmodul7_kelompok_8/Program.cs
+++ tpmodul7_kelompok_8/Program.cs
@@ -21,9 +21,32 @@ internal class Program
             Console.WriteLine("Nama " + deserialized.nama.depan + deserialized.nama.belakang + " dengan nim " + deserialized.nim + " dari fakultas " + deserialized.fakultas);
         }
     }
+
+    public class course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+    public class KuliahMahasiswa1302223153
+    {
+        public List<course> courses { get; set; }
+        public static void ReadJSON()
+        {
+            string json = File.ReadAllText(@"C:\Users\RAFLI\Downloads\Percodingan\C#\tpmodul7_kelompok_8\tp7_2_1302223153.json");
+            KuliahMahasiswa1302223153 deserialized = JsonSerializer.Deserialize<KuliahMahasiswa1302223153>(json);
+            int i = 1;
+            foreach(var MatKul in deserialized.courses)
+            {
+                Console.WriteLine("MK " + i + " " + MatKul.code + " - " + MatKul.name);
+                i++;
+            }
+        }
+    }
+    
     private static void Main(string[] args)
     {
         // Rafli
         DataMahasiswa1302223153.ReadJSON();
+        KuliahMahasiswa1302223153.ReadJSON();
     }
 }
