7446f4aa92f3b9abd321c819dd4609c2ce313e93
Rizky Kusuma Nugraha <kusumarizky65@gmail.com>
2024-03-31 20:04:52 +0700

    Menambah class KuliahMahasiswa1302220013

---
+++ tpmodul7_kelompok_8/KuliahMahasiswa1302220013.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class KuliahMahasiswa
+{
+    private string _fileName = "tp7_2_nim.json";
+
+    public void ReadJSON()
+    {
+        string jsonContent = File.ReadAllText(_fileName);
+        dynamic jsonObject = JsonConvert.DeserializeObject(jsonContent);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        int count = 1;
+        foreach (var course in jsonObject.courses)
+        {
+            Console.WriteLine($"MK {count} {course.code} - {course.name}");
+            count++;
+        }
+    }
+}
+
+class KuliahMahasiswa1302220013
+{
+    static void Main(string[] args)
+    {
+        KuliahMahasiswa kuliahMahasiswa = new KuliahMahasiswa();
+        kuliahMahasiswa.ReadJSON();
+    }
+}
